c6b4d98f6ab5303ed587b8026cb3af163ce033fc
NonoIceOff <nonocraft28@gmail.com>
2024-10-29 13:08:28 +0100

    Ajout de la connexion à la BDD, et des modeles

---
+++ lib/constant.dart
@@ -0,0 +1,7 @@
+const MONGO_CONN_URL = 
+  "mongodb+srv://flutterdb123:flutterdb123@cluster0.edmbw.mongodb.net/projet?retryWrites=true&w=majority&appName=Cluster0";
+const USER_COLLECTION = "user";
+const TEST_COLLECTION = "test";
+const QUESTION_COLLECTION = "question";
+const RESULTAT_TEST_COLLECTION = "resultat_test";
+const MOTIVATION_COLLECTION = "motivation";
---
+++ lib/models/testModel.dart
@@ -0,0 +1,18 @@
+import 'package:mongo_dart/mongo_dart.dart';
+
+class Test {
+  final String id;
+  final String nom_discipline;
+
+  Test({required this.id, required this.nom_discipline});
+
+  Map<String, dynamic> toJson() => {
+        '_id': ObjectId.fromHexString(id),
+        'nom_discipline': nom_discipline,
+      };
+
+  factory Test.fromJson(Map<String, dynamic> json) => Test(
+        id: (json['_id'] as ObjectId).toHexString(),
+        nom_discipline: json['nom_discipline'] ?? '',
+      );
+}
