89937083344cdb8bb4a315dfefe072e09984790d
NonoIceOff <nonocraft28@gmail.com>
2024-10-31 10:56:07 +0100

    passage des questions en qcm

--- lib/pages/createQuestion.dart
+++ lib/pages/createQuestion.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_cyclade/models/questionModel.dart';
 import 'package:flutter_cyclade/services/questionService.dart';
 import 'package:mongo_dart/mongo_dart.dart' show ObjectId;
@@ -20,8 +21,10 @@ class _CreateQuestionPageState extends State<CreateQuestionPage> {
   final TextEditingController _prop2Controller = TextEditingController();
   final TextEditingController _prop3Controller = TextEditingController();
   final TextEditingController _prop4Controller = TextEditingController();
-  final TextEditingController _correctAnswerController = TextEditingController();
   final TextEditingController _secondsController = TextEditingController();
+
+  // Liste de booléens pour garder les états des CheckBox de chaque réponse
+  List<bool> _reponseController = [false, false, false, false];
 
   // Fonction pour créer une question
   Future<void> _createQuestion() async {
@@ -30,7 +33,7 @@ class _CreateQuestionPageState extends State<CreateQuestionPage> {
     String prop2 = _prop2Controller.text.trim();
     String prop3 = _prop3Controller.text.trim();
     String prop4 = _prop4Controller.text.trim();
-    int correctAnswer = int.parse(_correctAnswerController.text.trim());
+    List<bool> reponse = _reponseController;
     int seconds = int.parse(_secondsController.text.trim());
 
     // Vérifie que tous les champs sont remplis
@@ -43,14 +46,14 @@ class _CreateQuestionPageState extends State<CreateQuestionPage> {
 
     // Crée une nouvelle instance de Question avec les valeurs saisies
     var newQuestion = Question(
-      id: ObjectId().toHexString(), // Génère un ID unique pour la question
-      id_test: widget.testId, // Associe la question au test en utilisant l'ID du test
+      id: ObjectId().toHexString(),
+      id_test: widget.testId,
       intitule: questionText,
       proposition_1: prop1,
       proposition_2: prop2,
       proposition_3: prop3,
       proposition_4: prop4,
-      reponse: correctAnswer,
+      reponse: reponse,
       seconds: seconds,
     );
 
@@ -58,7 +61,7 @@ class _CreateQuestionPageState extends State<CreateQuestionPage> {
     String result = await QuestionService.createQuestion(newQuestion);
     ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(result)));
 
-    Navigator.pop(context, newQuestion); // Ferme la page et renvoie la nouvelle question
+    Navigator.pop(context, newQuestion);
   }
 
   // Construction de l'interface utilisateur pour la page
@@ -70,12 +73,10 @@ class _CreateQuestionPageState extends State<CreateQuestionPage> {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            // Champ de saisie pour l'intitulé de la question
             TextField(
               controller: _questionTextController,
               decoration: const InputDecoration(labelText: "Intitulé de la question"),
             ),
-            // Champ de saisie pour chaque proposition de réponse
             TextField(
               controller: _prop1Controller,
               decoration: const InputDecoration(labelText: "Proposition 1"),
@@ -92,22 +93,54 @@ class _CreateQuestionPageState extends State<CreateQuestionPage> {
               controller: _prop4Controller,
               decoration: const InputDecoration(labelText: "Proposition 4"),
             ),
-            // Champ de saisie pour la réponse correcte (doit être entre 1 et 4)
-            TextField(
-              controller: _correctAnswerController,
-              decoration: const InputDecoration(labelText: "Réponse correcte (1-4)"),
-              keyboardType: TextInputType.number,
+            const SizedBox(height: 20),
+            const Text("Sélectionnez les bonnes réponses :"),
+            // Choix des réponses pour le qcm
+            CheckboxListTile(
+              title: const Text("Proposition 1"),
+              value: _reponseController[0],
+              onChanged: (bool? value) {
+                setState(() {
+                  _reponseController[0] = value ?? false;
+                });
+              },
             ),
-            // Champ de saisie pour la durée de la question
+            CheckboxListTile(
+              title: const Text("Proposition 2"),
+              value: _reponseController[1],
+              onChanged: (bool? value) {
+                setState(() {
+                  _reponseController[1] = value ?? false;
+                });
+              },
+            ),
+            CheckboxListTile(
+              title: const Text("Proposition 3"),
+              value: _reponseController[2],
+              onChanged: (bool? value) {
+                setState(() {
+                  _reponseController[2] = value ?? false;
+                });
+              },
+            ),
+            CheckboxListTile(
+              title: const Text("Proposition 4"),
+              value: _reponseController[3],
+              onChanged: (bool? value) {
+                setState(() {
+                  _reponseController[3] = value ?? false;
+                });
+              },
+            ),
             TextField(
               controller: _secondsController,
-              decoration: const InputDecoration(labelText: "Durée de la question"),
+              decoration: const InputDecoration(labelText: "Durée en secondes"),
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly], // Limite aux chiffres
               keyboardType: TextInputType.number,
             ),
-            const SizedBox(height: 20), // Espacement avant le bouton
-            // Bouton pour soumettre la question
+            const SizedBox(height: 20),
             ElevatedButton(
-              onPressed: _createQuestion, // Appelle _createQuestion lors de la soumission
+              onPressed: _createQuestion,
               child: const Text("Créer la question"),
             ),
           ],
